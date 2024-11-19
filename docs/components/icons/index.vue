0f140e1c9539260453903933e8813840d8ed3e20
lhdu <lhdu@bigquant.ai>
2024-11-19 20:54:54 +0800

    文件位置迁移&ci

---
+++ docs/components/icons/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :class="$style.box">
+    <div
+      v-for="item in icons"
+      :key="item.name"
+      :class="$style.item"
+      @click="handleCopy(item.name)"
+    >
+      <component :is="item" :class="$style.icon" />
+      <div :class="$style.name">{{ item.name }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import * as a from "bigquant-icons-vue";
+import { message } from "ant-design-vue";
+
+const icons = Object.values(a);
+
+const handleCopy = (name) => {
+  const text = `<${name} />`;
+  navigator.clipboard.writeText(text);
+  message.success(`${text} Copied!`);
+};
+</script>
+
+<style module>
+.box {
+  margin-top: 20px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 100px);
+  gap: 24px;
+}
+.item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+.icon {
+  width: 24px;
+  height: 24px;
+}
+.name {
+  margin-top: 8px;
+  font-size: 12px;
+  color: #666;
+}
+</style>
---
+++ docs/icons.md
@@ -0,0 +1,8 @@
+# Icons
+
+<Icons />
+
+<script setup>
+import Icons from "./components/icons/index.vue"
+</script>
+
